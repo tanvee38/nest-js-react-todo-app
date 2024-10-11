ed31bb765f5c5b676a7e50cca0e75f2b7801181e
Moin <moin.mahmud38@gmail.com>
2024-10-10 22:43:45 -0600

    update styling for front end

--- todo-web-app/src/components/layout.tsx
+++ todo-web-app/src/components/layout.tsx
@@ -5,7 +5,7 @@ export function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-            <Link className="navbar-brand" to="/">WebApp</Link>
+            <Link className="navbar-brand" to="/">Todo App</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
--- todo-web-app/src/index.tsx
+++ todo-web-app/src/index.tsx
@@ -1,13 +1,11 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Navbar, Footer} from './components/layout';
 import { Home } from './components/home';
 import { Todos } from './components/TodoApp';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import './todo.css';
 
 function App() {
   return (
