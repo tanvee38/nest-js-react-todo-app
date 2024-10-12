64d062569b028b81399e983878563c288faf7e8a
Moin <moin.mahmud38@gmail.com>
2024-10-12 13:09:11 -0600

    Update home pace

--- todo-web-app/src/components/home.tsx
+++ todo-web-app/src/components/home.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Home() {
     return (
         <div  className="container my-5">
-            <h2 className="text-center mb-3">Home Page</h2>
+            <h2 className="text-center mb-3">This is a simple Todo app made with react</h2>
         </div>
     );
 }
--- todo-web-app/src/components/layout.tsx
+++ todo-web-app/src/components/layout.tsx
@@ -6,9 +6,6 @@ export function Navbar() {
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
             <Link className="navbar-brand" to="/">Todo App</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
